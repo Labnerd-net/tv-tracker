a6a4c9364b8866bb19a7d194dddc6502e39fb9cd
Brian Ladner <git@theladners.com>
2026-03-28 11:19:23 -0500

    perf: lazy-load route-level page components to reduce initial bundle size

--- apps/ui/src/AppContent.tsx
+++ apps/ui/src/AppContent.tsx
@@ -1,15 +1,17 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router';
 import AppAlert from './components/AppAlert.tsx';
 import Navbar from './components/Navbar.tsx';
-import Splash from './pages/Splash.tsx';
-import Login from './pages/Login.tsx';
-import Registration from './pages/Registration.tsx';
-import AllShows from './pages/AllShows.tsx';
-import OneShow from './pages/OneShow.tsx';
-import SearchResults from './pages/SearchResults.tsx';
-import OneShowSearch from './pages/OneShowSearch.tsx';
 import ProtectedRoute from './pages/ProtectedRoute.tsx';
 import { useAlert } from './contexts/alert/AlertContext.tsx';
+
+const Splash = lazy(() => import('./pages/Splash.tsx'));
+const Login = lazy(() => import('./pages/Login.tsx'));
+const Registration = lazy(() => import('./pages/Registration.tsx'));
+const AllShows = lazy(() => import('./pages/AllShows.tsx'));
+const OneShow = lazy(() => import('./pages/OneShow.tsx'));
+const SearchResults = lazy(() => import('./pages/SearchResults.tsx'));
+const OneShowSearch = lazy(() => import('./pages/OneShowSearch.tsx'));
 
 export default function AppContent() {
   const alertProps  = useAlert();
@@ -18,15 +20,17 @@ export default function AppContent() {
     <>
       <Navbar />
       {alertProps.visibleAlert && <AppAlert alertVariant={alertProps.alertVariant} alertMessage={alertProps.alertMessage} />}
-      <Routes>
-        <Route path='/' element={<Splash />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Registration />} />
-        <Route path='/dashboard' element={<ProtectedRoute><AllShows /></ProtectedRoute>} />
-        <Route path='/tvshow/:showID' element={<ProtectedRoute><OneShow /></ProtectedRoute>} />
-        <Route path='/search/:showName' element={<ProtectedRoute><SearchResults /></ProtectedRoute>} />
-        <Route path='/search/show/:showID' element={<ProtectedRoute><OneShowSearch /></ProtectedRoute>} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path='/' element={<Splash />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Registration />} />
+          <Route path='/dashboard' element={<ProtectedRoute><AllShows /></ProtectedRoute>} />
+          <Route path='/tvshow/:showID' element={<ProtectedRoute><OneShow /></ProtectedRoute>} />
+          <Route path='/search/:showName' element={<ProtectedRoute><SearchResults /></ProtectedRoute>} />
+          <Route path='/search/show/:showID' element={<ProtectedRoute><OneShowSearch /></ProtectedRoute>} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
